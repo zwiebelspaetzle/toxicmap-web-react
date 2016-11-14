a14c4b2fdba778dfdd669327e9c30ec6656b44f2
Chris Ward <cward@Chris-Ward-MBP.local>
2016-11-14 16:18:07 -0700

    organization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './toxicmap.css';
+import App from './components/App';
+import './css/toxicmap.css';
 
 ReactDOM.render(
   <App />,
